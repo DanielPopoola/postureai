46d5b731922826cc16248f1def8317a459ab5e4e
Daniel Popoola <iamuchihadaniel236@gmail.com>
2026-02-24 20:42:44 +0100

    feat: updated favicon

--- frontend/src/hooks/useNotifications.ts
+++ frontend/src/hooks/useNotifications.ts
@@ -16,8 +16,8 @@ export function useNotifications() {
 
     new Notification(title, {
       body,
-      icon: "/favicon.ico",
-      badge: "/favicon.ico",
+      icon: "/favicon.svg",
+      badge: "/favicon.svg",
     });
   }, []);
 
